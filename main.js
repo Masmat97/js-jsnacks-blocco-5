0132b079c613efb6e4081154f91a219bf52c56bf
Matteo Mascio <matteo.mascio@libero.it>
2024-05-10 17:59:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,169 @@
+const zucchine = [
+    {
+        name: "verde",
+        lunghezza: "10",
+        peso: 2,
+    }, {
+        name: "giallo",
+        lunghezza: "10",
+        peso: 3,
+    }, {
+        name: "rosso",
+        lunghezza: "25",
+        peso: 4,
+    }, {
+        name: "blu",
+        lunghezza: "14",
+        peso: 6,
+    }, {
+        name: "nero",
+        lunghezza: "30",
+        peso: 2,
+    }, {
+        name: "viola",
+        lunghezza: "20",
+        peso: 1,
+    }, {
+        name: "arancio",
+        lunghezza: "27",
+        peso: 3,
+    }, {
+        name: "bianco",
+        lunghezza: "10",
+        peso: 4,
+    }, {
+        name: "nero",
+        lunghezza: "52",
+        peso: 7,
+    }, {
+        name: "marrone",
+        lunghezza: "30",
+        peso: 4,
+    },
+];
+// Primo snak
+// let pesoGruppo = 0;
+
+// // function addizione() {
+
+//     for (let i = 0; i < zucchine.length; i++) {
+//         let pesoSingolo = zucchine[i].peso;
+//         pesoGruppo = pesoGruppo + pesoSingolo;
+
+        
+//     };
+//     console.log(pesoGruppo);
+//     // return "Il peso finale è" + pesoGruppo;
+
+// // };
+// // addizione();
+
+
+
+
+// Secondo snack
+
+// let zucchineCorte = zucchine.filter(zucchina => zucchina.lunghezza < 15);
+// let zucchineLunghe = zucchine.filter(zucchina => zucchina.lunghezza >= 15);
+//  console.log(zucchineCorte);
+//  console.log(zucchineLunghe);
+ 
+//  let pesoTotale = 0;
+
+//  for (let i = 0 ; i < zucchineCorte.length; i++){
+//     let pesoSingolo = zucchineCorte[i].peso;
+//     pesoTotale = pesoTotale + pesoSingolo;
+//  };
+//  console.log("Il peso é", pesoTotale);
+
+//  let pesoTotale2 = 0;
+
+//  for (let i = 0 ; i < zucchineLunghe.length; i++){
+//     let pesoSingolo2 = zucchineLunghe[i].peso;
+//     pesoTotale2 = pesoTotale2 + pesoSingolo2;
+//  };
+//  console.log("Il peso è:", pesoTotale2);
+
+
+
+
+
+// Terzo snack
+
+// function invertiStringa(stringa) {
+//     return stringa.split('').reverse().join('');
+//   }
+  
+//   // Esempio di utilizzo
+//   let stringaOriginale = "Ciao";
+//   let stringaInvertita = invertiStringa(stringaOriginale);
+//   console.log(stringaInvertita); 
+
+
+// const parola = "ciao";
+// const ultimoCarattere = parola.length - 1;
+// let parolaAlContrario = "";
+
+
+// for(let i = ultimoCarattere; parola.length; i--){
+// const element = parola[i];
+
+// parolaAlContrario += ultimoCarattere;
+// };
+// console.log(element)
+
+
+
+// Quarto snack
+
+// function fondaArray(array1, array2) {
+//     let result = [];
+//     for (let i = 0; i < array1.length; i++) {
+//       result.push(array1[i]);
+//       result.push(array2[i]);
+//     }
+//     return result;
+//   }
+// // Esempio di utilizzo
+// let array1 = ['a', 'b', 'c'];
+// let array2 = [1, 2, 3];
+// let arrayFuso = fondaArray(array1, array2);
+// console.log(arrayFuso); 
+
+
+
+
+// const primo = ["a", "b", "c"];
+// const secondo = [1, 2, 3];
+// const risultato = [];
+
+// for (let i = 0; i < primo.length; i++){
+//     risultato.push(primo[i], secondo[i]);
+// }
+// console.log(risultato);
+
+// const array1 = [`a`, `b`, `c`];
+// const array2 = [1, 2, 3, 4, 5];
+// function arrayFusion(x, y) {
+//     let arrayFused = [];
+//     let n = array1.length;
+//     if (array1.length < array2.length) {
+//         n = array2.length;
+//     }
+//     for (let i = 0; i < n; i++) {
+//         if (x[i] != undefined) {
+//             arrayFused.push(x[i]);
+//         }
+//         if (y[i] != undefined) {
+//             arrayFused.push(y[i]);
+//         }
+//     }
+//     console.log(arrayFused);
+// }
+// arrayFusion(array1, array2);
+
+
+
+// Quinto snack
+
+
